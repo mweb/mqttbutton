eda4be6973808736be55f0aec52b3103ddba9c19
Mathias Weber <mathew.weber@gmail.com>
2016-05-11 21:01:00 +0200

    code clean up, better nameing for PIN const expressions

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <FS.h>
 #include <ESP8266WiFi.h> // https://github.com/esp8266/Arduino
 
-//#include <DNSServer.h>
-//#include <ESP8266WebServer.h>
-
 #include "mqtthandler.h"
 #include "confighandler.h"
 
-constexpr int8_t PIN_01 = 0;
-constexpr int8_t PIN_02 = 1;
-constexpr int8_t PIN_03 = 3;
-constexpr int8_t PIN_04 = 4;
-constexpr int8_t PIN_05 = 5;
-constexpr int8_t PIN_06 = 12;
-constexpr int8_t PIN_07 = 13;
-constexpr int8_t PIN_08 = 14;
-constexpr int8_t PIN_09 = 15;
-constexpr int8_t PIN_19 = 16;
+constexpr int8_t PIN_D0 = 16;
+constexpr int8_t PIN_D1 = 5;
+constexpr int8_t PIN_D2 = 4;
+constexpr int8_t PIN_D3 = 0;
+constexpr int8_t PIN_D4 = 2;
+constexpr int8_t PIN_D5 = 14;
+constexpr int8_t PIN_D6 = 12;
+constexpr int8_t PIN_D7 = 13;
+constexpr int8_t PIN_D8 = 15;
+constexpr int8_t PIN_RX = 3;
+constexpr int8_t PIN_TX = 1;
+constexpr int8_t PIN_SD3 = 10;
+constexpr int8_t PIN_SD2 = 9;
 
 uint16_t status = 0;
 
@@ -35,20 +35,19 @@ void interruptFour() { status |= 1 << 3; }
 
 /** Setup all the inputs and outputs */
 void setupIOPins() {
-    pinMode(PIN_01, OUTPUT);
-    pinMode(PIN_02, OUTPUT);
-    pinMode(PIN_03, OUTPUT);
-    pinMode(PIN_04, OUTPUT);
+    pinMode(PIN_D1, OUTPUT);
+    pinMode(PIN_D2, OUTPUT);
+    pinMode(PIN_D3, OUTPUT);
 
-    pinMode(PIN_05, INPUT_PULLUP);
-    pinMode(PIN_06, INPUT_PULLUP);
-    pinMode(PIN_07, INPUT_PULLUP);
-    pinMode(PIN_08, INPUT_PULLUP);
+    pinMode(PIN_D5, INPUT_PULLUP);
+    pinMode(PIN_D6, INPUT_PULLUP);
+    pinMode(PIN_D7, INPUT_PULLUP);
+    pinMode(PIN_D8, INPUT_PULLUP);
 
-    attachInterrupt(digitalPinToInterrupt(PIN_05), interruptOne, FALLING);
-    attachInterrupt(digitalPinToInterrupt(PIN_06), interruptTwo, FALLING);
-    attachInterrupt(digitalPinToInterrupt(PIN_07), interruptThree, FALLING);
-    attachInterrupt(digitalPinToInterrupt(PIN_08), interruptFour, FALLING);
+    attachInterrupt(digitalPinToInterrupt(PIN_D5), interruptOne, FALLING);
+    attachInterrupt(digitalPinToInterrupt(PIN_D6), interruptTwo, FALLING);
+    attachInterrupt(digitalPinToInterrupt(PIN_D7), interruptThree, FALLING);
+    attachInterrupt(digitalPinToInterrupt(PIN_D8), interruptFour, FALLING);
 }
 
 /** The setup entry point, gets called on start up */
